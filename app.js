21c1ee58cf9398773685f9204b60fbd8c72b1bba
aelumbreras <aelumbreras>
2016-12-16 15:08:49 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-  var express = require('express');
+	var express = require('express');
   var app = express();
 
- app.set('port', (process.env.PORT || 5000));
- app.use(express.static(__dirname + '/Public'));
+  var port = Number(process.env.PORT || 3000);
 
- app.get('/', function(request, response) {
- response.send('Hello World!');
- });
+  app.listen(port, function()
+   {
+    console.log('Listening on ' + port);
+   });
+
+  app.use(express.static(__dirname  + '/Public'));
