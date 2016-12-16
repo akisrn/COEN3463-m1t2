67b2a5142d8668bbd3b8774878abf411b280ff32
aelumbreras <aelumbreras>
2016-12-16 15:29:55 +0800

    second commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var app = express();
 
 app.set('port', (process.env.PORT || 8888));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.listen(app.get('port'), function() {
     console.log('Expres server is running on port 8888!');
